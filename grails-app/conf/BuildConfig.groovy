864ebe64d93d37bed22964df0779c8c301dadf85
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2013-08-20 13:36:28 +0300

    hibernate kaldirildi

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,7 +45,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+//        runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
 
@@ -56,7 +56,7 @@ grails.project.dependency.resolution = {
 
         build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.3.2"
+//        runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
     }
